7d5286faa881d53c24b9ff172a00846b93916d51
irisgroen <iris.groen@nyu.edu>
2018-05-23 14:47:44 -0400

    updated stimcodes for dottask experiment

--- stimMakeTaskExperiment.m
+++ stimMakeTaskExperiment.m
@@ -15,14 +15,15 @@ stimulus = [];
 stimulus.cmap       = stimParams.stimulus.cmap;
 stimulus.srcRect    = stimParams.stimulus.srcRect;
 stimulus.dstRect    = stimParams.stimulus.destRect;
+stimulus.display    = stimParams.display;
+
 stimulus.images     = stimParams.stimulus.images(:,:,end);
+stimulus.cat        = [134 135];
+stimulus.categories = {'TASK', 'REST'};
+
 stimulus.seqtiming  = 0:1/frameRate:experimentLength;
 stimulus.seq        = ones(size(stimulus.seqtiming));
 stimulus.fixSeq     = ones(size(stimulus.seqtiming));
-
-
-
-
 
 idx = mod(stimulus.seqtiming,blockLength*2)<blockLength;
 stimulus.fixSeq(idx) = 3;
@@ -30,7 +31,6 @@ stimulus.fixSeq(idx) = 3;
 % insert blips
 minISI    = 1.5; % seconds
 maxISI    = 8;
-
 
 fixSeq = createFixationSequence(stimulus, 1/frameRate, minISI, maxISI);
 blips  = abs(diff([1 fixSeq]))>0;
@@ -47,27 +47,28 @@ switch lower(stimParams.modality)
         stimulus.trigSeq   = blips;
 end
 
-
+% Sparsify stimulus sequence
 maxUpdateInterval = 0.25;
 stimulus = sparsifyStimulusStruct(stimulus, maxUpdateInterval);
-
 
 % Create stim_file name
 fname = sprintf('%s_dottask_%d.mat', site, runNum);
 
-
 % Add table with elements to write to tsv file for BIDS
 conditions  = {'task' 'rest'};
-    
+condition_idx = mod(0:numBlocks-1,2)+1;
+
 onset       = ((0:numBlocks-1)*blockLength)';
 duration    = ones(size(onset)) * blockLength;
-trial_type  = (mod(0:numBlocks-1,2)+1)';
-trial_name  = conditions(trial_type)';
+trial_type  = stimulus.cat(condition_idx)';
+trial_name  = conditions(condition_idx)';
 stim_file   = repmat(fname, length(onset),1);
 stim_file_index = ones(size(onset));
 
 stimulus.tsv = table(onset, duration, trial_type, trial_name, stim_file, stim_file_index);
 
+% Save
+fprintf('[%s]: Saving stimuli in: %s\n', mfilename, fullfile(vistadispRootPath, 'StimFiles',  fname));
 save(fullfile(vistadispRootPath, 'StimFiles',  fname), 'stimulus')
 
 end
