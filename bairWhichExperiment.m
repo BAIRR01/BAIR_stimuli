0788f7d5905b9b336d2606515c6f2a6903278a13
Stephanie Montenegro <smontenegro26@gmail.com>
2019-01-09 11:52:53 -0500

    added 4 motor tasks

--- bairWhichExperiment.m
+++ bairWhichExperiment.m
@@ -2,7 +2,8 @@ function [experimentType, selectionMade] = bairWhichExperiment
 
 % Which experiment?
 experimentTypes = {'DOTTASK' 'PRF' 'SPATIALPATTERN' 'SPATIALOBJECT' ...
-    'TEMPORALPATTERN' 'HRFPATTERN' 'HRFPATTERNBREATHINGCHALLENGE','TACTILE', 'MOTOR' };
+    'TEMPORALPATTERN' 'HRFPATTERN' 'HRFPATTERNBREATHINGCHALLENGE','TACTILE',...
+    'FINGERMAPPING', 'GESTURES', 'BOLDHAND', 'BOLDSAT' };
 % 'HRFCHECKER'  'HRFPATTERNINVERTED' 'HRFCHECKERINVERTED' };
 [whichExperiment, selectionMade] = listdlg('PromptString', 'Which experiment?', 'ListString', experimentTypes);
 
