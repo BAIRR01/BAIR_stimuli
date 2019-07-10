cb09e65dce7854c900fa1451332d6158e5a54e1d
JWinawer <jonathan.winawer@nyu.edu>
2019-07-10 17:09:23 -0400

    debugged real time plotting as well as added ability to use keypress to quit

--- dataGloveRecordContinuously.m
+++ dataGloveRecordContinuously.m
@@ -3,33 +3,51 @@ if ~ssDefined, return; end
 
 glovePointer = initializeDataGlove();
 
-t0 = GetSecs();  
+t0 = GetSecs();
 sampleTime = 2/60;
 counter = 1;
 
 pth = fullfile(vistadispRootPath, 'Data');
 fname = sprintf('sub-%s_ses-nyumeg%2d_DataGlove_%s',subjID, sessionID, datestr(now, 'yyyy_mm_dd_hh_MM_ss'));
 
-fid = fopen(fullfile(pth, fname) ,'w'); 
+fid = fopen(fullfile(pth, fname) ,'w');
 
-figure;
-hold on;
+f = figure; hold on
+for ff = 1:5
+    f(ff) = animatedline;
+end
+recordData = true;
 
-while true
+while recordData
     recordTime = sampleTime * counter;
     t = GetSecs() - t0;
     WaitSecs(recordTime - t);
-
     data = sampleDataglove (glovePointer);
+    
     
     fprintf(fid,  '%s\t', datestr(now, 'hh_MM_ss_FFF'));
     for ii = 1:length(data)
-       fprintf(fid,  '%d\t', data(ii));      
+        fprintf(fid,  '%d\t', data(ii));
     end
-    
     fprintf(fid, '\n');
- plot (data);
+    for ff = 1:5
+        addpoints(f(ff), counter, data(1,ff))
+    end
+    drawnow
     counter = counter + 1;
+    
+    [ssKeyIsDown,~,ssKeyCode] = KbCheck(-1);
+    
+    if ssKeyIsDown
+        str = KbName(find(ssKeyCode));
+        if iscell(str)
+            str = str{1};
+        end
+        str = str(1);
+        if str == 'q'
+            recordData = false;
+        end
+    end
 end
 
 fclose(fid);
