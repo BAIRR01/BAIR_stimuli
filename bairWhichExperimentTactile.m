35887b589abfd7b949e76f56c97f1a19712c0f5e
StephanieBadde <80488967+StephanieBadde@users.noreply.github.com>
2021-03-13 13:37:19 -0500

    added experiments
    the temporal and spatial experiment are now based on pre-optimized stimulus sequences (which use the current settings, see google doc)

--- bairWhichExperimentTactile.m
+++ bairWhichExperimentTactile.m
@@ -1,7 +1,7 @@
 function [experimentType, selectionMade] = bairWhichExperimentTactile
 
 % Which experiment?
-experimentTypes = {'SIMPLEHANDSWEEP', 'BLOCKED', 'TACTILEVISUALSWEEP', 'TACTILEVISUALBLOCKED', 'RANDOM_5FINGERS', 'TEMPORAL', 'STIMULUSTEST'};
+experimentTypes = {'SIMPLEHANDSWEEP', 'BLOCKED', 'TACTILEVISUALSWEEP', 'TACTILEVISUALBLOCKED', 'RANDOM_5FINGERS', 'TEMPORAL', 'TACTILEMAPPING', 'STIMULATORTEST'};
 % 'HRFCHECKER'  'HRFPATTERNINVERTED' 'HRFCHECKERINVERTED' };
 [whichExperiment, selectionMade] = listdlg('PromptString', 'Which experiment?', 'ListString', experimentTypes);
 
